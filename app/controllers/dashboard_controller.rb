8b23256eb2b43d0327c33d453bd292e212f60b4a
addhyanmalhotra <addhyanmalhotra@gmail.com>
2021-03-09 11:41:23 +0530

    Added Auth and a lot of CSS

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -9,8 +9,8 @@ class DashboardController < ApplicationController
     @col_chart_data = [["Branch Name", "Profit"],["Month Average Profit",avg_monthly_profit], ["Net Monthly Profit",net_monthly_profit]]
     hotel_name_hash = Hotel.where(id: @hotel_wise_monthly_expenses.pluck(:branch_id)).pluck(:id,:branch_name).to_h
     @hotel_wise_monthly_expenses.each do |me|
-      @bar_chart_data.append([hotel_name_hash[me.branch_id],me.monthly_turnover])
-      @col_chart_data.append([hotel_name_hash[me.branch_id],me.monthly_profit])
+      @bar_chart_data.append([hotel_name_hash[me.branch_id],me.monthly_turnover || 0])
+      @col_chart_data.append([hotel_name_hash[me.branch_id],me.monthly_profit || 0])
     end
 
 
